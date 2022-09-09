585362295b3d25ffe4f83404f2ff7c0f40bb2f7d
Bippy008 <62377483+Bippy008@users.noreply.github.com>
2022-09-09 21:54:53 +0600

    дз 4 месяц котлин 2

--- app/src/main/java/kg/geektech/newsapp46/ui/profile/ProfileFragment.kt
+++ app/src/main/java/kg/geektech/newsapp46/ui/profile/ProfileFragment.kt
@@ -9,14 +9,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.ActivityResultRegistry
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import kg.geektech.newsapp46.MainActivity
 import kg.geektech.newsapp46.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
+    private lateinit var launcher: ActivityResultLauncher<Intent>
 
     private val binding get() = _binding!!
 
@@ -31,19 +34,31 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.btnChooseImage.setOnClickListener { openGallery() }
+        initLauncher()
+        binding.btnChooseImage.setOnClickListener {
+            openGallery()
+        }
     }
 
     private fun openGallery() {
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        startActivityForResult(intent, MainActivity.IMAGE_REQUEST_CODE)
+        val intentAvatar = Intent(Intent.ACTION_PICK)
+        intentAvatar.type = "image/*"
+        launcher.launch(intentAvatar)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK && requestCode == MainActivity.IMAGE_REQUEST_CODE) {
             binding.profileImage.setImageURI(data?.data)
+        }
+    }
+
+    private fun initLauncher() {
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == AppCompatActivity.RESULT_OK) {
+                val image = it.data?.data
+                binding.profileImage.setImageURI(image)
+            }
         }
     }
 }
